4dc7095a0f7a64c08ec3c2f68c0d934b2edfcac1
Gerzson Pszota <gerzson.pszota@gmail.com>
2024-02-07 19:44:45 +0100

    feat: Block class fields & constructor

---
+++ src/Block.java
@@ -0,0 +1,15 @@
+import java.util.Date;
+
+public class Block {
+
+  public String hash; // digital signature
+  public String previousHash; // previous block's hash
+  private String data; // block data
+  private Long timeStamp; // as number of milliseconds since 1/1/1970
+
+  public Block(String data, String previousHash) {
+    this.data = data;
+    this.previousHash = previousHash;
+    this.timeStamp = new Date().getTime();
+  }
+}
