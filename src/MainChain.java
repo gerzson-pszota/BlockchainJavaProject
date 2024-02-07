41abb472a8b70da8bab9d9252fb838615756e437
Gerzson Pszota <gerzson.pszota@gmail.com>
2024-02-07 21:12:24 +0100

    feat: adding isChainValid( ) to check integrity

--- src/MainChain.java
+++ src/MainChain.java
@@ -8,10 +8,34 @@ public class MainChain {
   public static void main(String[] args) {
 
     blockchain.add(new Block("Hi, I am the first block!", "0"));
-    blockchain.add(new Block("Yo, I am the second block!", blockchain.get(blockchain.size() - 1).hash));
-    blockchain.add(new Block("Hey, I am the third block!", blockchain.get(blockchain.size() - 1).hash));
+    blockchain.add(
+        new Block("Yo, I am the second block!", blockchain.get(blockchain.size() - 1).hash));
+    blockchain.add(
+        new Block("Hey, I am the third block!", blockchain.get(blockchain.size() - 1).hash));
 
     String blockchainJson = new GsonBuilder().setPrettyPrinting().create().toJson(blockchain);
     System.out.println(blockchainJson);
+  }
+
+  public static Boolean isChainValid() {
+    Block currentBlock;
+    Block previousBlock;
+
+    // loop through blockchain to check hashes:
+    for (int i = 1; i < blockchain.size(); i++) {
+      currentBlock = blockchain.get(i);
+      previousBlock = blockchain.get(i - 1);
+      // compare registered hash and calculated hash
+      if (!currentBlock.hash.equals(currentBlock.calculateHash())) {
+        System.out.println("Current Hashes not equal");
+        return false;
+      }
+      // compare previous hash and registered previous hash
+      if (!previousBlock.hash.equals(currentBlock.previousHash)) {
+        System.out.println("Previous Hashes not equal");
+        return false;
+      }
+    }
+    return true;
   }
 }
