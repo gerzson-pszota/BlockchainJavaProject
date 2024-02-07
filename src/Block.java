1db753fdd98ed15a489a176dc43f569e160cad64
Gerzson Pszota <gerzson.pszota@gmail.com>
2024-02-07 21:46:19 +0100

    feat: 'proof of work' mining

--- src/Block.java
+++ src/Block.java
@@ -5,7 +5,8 @@ public class Block {
   public String hash; // digital signature
   public String previousHash; // previous block's hash
   private String data; // block data
-  private Long timeStamp; // as number of milliseconds since 1/1/1970
+  private long timeStamp; // as number of milliseconds since 1/1/1970
+  private int nonce;
 
   public Block(String data, String previousHash) {
     this.data = data;
@@ -15,6 +16,19 @@ public class Block {
   }
 
   public String calculateHash() {
-    return StringUtil.applySha256(previousHash + Long.toString(timeStamp) + data);
+    return StringUtil.applySha256(
+        previousHash + Long.toString(timeStamp) + Integer.toString(nonce) + data);
+  }
+
+  // I use 4–6 for testing. (At the time of coding, Litecoin’s difficulty is around 32.67 Million)
+  public void mineBlock(int difficulty) {
+    String target =
+        new String(new char[difficulty])
+            .replace('\0', '0'); // Create a string with difficulty * "0"
+    while (!hash.substring(0, difficulty).equals(target)) {
+      nonce++;
+      hash = calculateHash();
+    }
+    System.out.println("Block mined!!! : " + hash);
   }
 }
--- src/MainChain.java
+++ src/MainChain.java
@@ -4,22 +4,35 @@ import com.google.gson.GsonBuilder;
 public class MainChain {
 
   public static ArrayList<Block> blockchain = new ArrayList<Block>();
+  public static int difficulty = 5;
 
   public static void main(String[] args) {
 
     blockchain.add(new Block("Hi, I am the first block!", "0"));
+    System.out.println("Trying to mine block 1... ");
+    blockchain.get(0).mineBlock(difficulty);
+
     blockchain.add(
         new Block("Yo, I am the second block!", blockchain.get(blockchain.size() - 1).hash));
+    System.out.println("Trying to mine block 2... ");
+    blockchain.get(1).mineBlock(difficulty);
+
     blockchain.add(
         new Block("Hey, I am the third block!", blockchain.get(blockchain.size() - 1).hash));
+    System.out.println("Trying to mine block 3... ");
+    blockchain.get(2).mineBlock(difficulty);
+
+    System.out.println("\nBlockchain is Valid: " + isChainValid());
 
     String blockchainJson = new GsonBuilder().setPrettyPrinting().create().toJson(blockchain);
+    System.out.println("\nThe blockchain: ");
     System.out.println(blockchainJson);
   }
 
   public static Boolean isChainValid() {
     Block currentBlock;
     Block previousBlock;
+    String hashTarget = new String(new char[difficulty]).replace('\0', '0');
 
     // loop through blockchain to check hashes:
     for (int i = 1; i < blockchain.size(); i++) {
