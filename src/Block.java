06aabb42c8c8fbb891b56ec92adefea95bf0d5e6
Gerzson Pszota <gerzson.pszota@gmail.com>
2024-02-07 20:13:46 +0100

    feat: adding calculateHash( );

--- src/Block.java
+++ src/Block.java
@@ -11,5 +11,10 @@ public class Block {
     this.data = data;
     this.previousHash = previousHash;
     this.timeStamp = new Date().getTime();
+    this.hash = calculateHash();
+  }
+
+  public String calculateHash() {
+    return StringUtil.applySha256(previousHash + Long.toString(timeStamp) + data);
   }
 }
