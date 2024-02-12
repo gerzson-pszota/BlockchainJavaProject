4f7e69e55bd60434c99b3f9720ebb4015630a6cd
Gerzson Pszota <gerzson.pszota@gmail.com>
2024-02-12 12:05:52 +0100

    refactor: adding comments

--- src/transaction/Transaction.java
+++ src/transaction/Transaction.java
@@ -7,16 +7,16 @@ import java.util.ArrayList;
 
 public class Transaction {
 
-  public String transactionId;
-  public PublicKey sender;
-  public PublicKey reciepient;
+  public String transactionId; // this is also the hash of the transaction.
+  public PublicKey sender; // senders address/public key.
+  public PublicKey reciepient; // Recipients address/public key.
   public float value;
-  public byte[] signature;
+  public byte[] signature; // this is to prevent anybody else from spending funds in our wallet.
 
   public ArrayList<TransactionInput> inputs = new ArrayList<TransactionInput>();
   public ArrayList<TransactionOutput> outputs = new ArrayList<TransactionOutput>();
 
-  private static int sequence = 0;
+  private static int sequence = 0; // a rough count of how many transactions have been generated.
 
   public Transaction(
       PublicKey from, PublicKey to, float value, ArrayList<TransactionInput> inputs) {
@@ -26,6 +26,7 @@ public class Transaction {
     this.inputs = inputs;
   }
 
+  // this calculates the transaction hash (which will be used as its Id)
   private String calculateHash() {
     sequence++;
     return StringUtil.applySha256(
